817a5572a63d0311f57fa21edb12af60d4f84d20
DOMINGUEZ Carlos Enrique <CarlosEDomin@essilorluxottica.id>
2024-04-08 17:13:12 -0600

    initial commit

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,9 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css',
+})
+export class LazyImageComponent { }
